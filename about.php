91f121432b5feeb8bd94153c982679ab9d89a436
brat-maksim <brat.maksim@gmail.com>
2017-07-14 23:42:10 +0300

    добавлен код php и контент для body

--- about.php
+++ about.php
@@ -1,11 +1,46 @@
+
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
-	<link rel="stylesheet" href="css/styles.css">
+	<title>Рустам</title>
+	<style type="text/css">
+		
+	</style>
 </head>
 <body>
-	
+	<?php
+	$userName = 'Рустам';
+	$age = '24';
+	$email = 'r-guseynov@gmail.com';
+	$city = 'Санкт-Петербург';
+	$about = 'Тренер по армспорту';
+	echo $userName;
+	?>
+
+	<h1>Страница пользователя Рустам</h1>	
+	<dl>
+		<dt>Имя</dt>
+		<dd><?= $userName ?></dd>
+	</dl>
+	<dl>
+		<dt>Возраст</dt>
+		<dd><?= $age ?></dd>
+	</dl>
+	<dl>
+		<dt>Адрес электронной почты</dt>
+		<dd>
+			<a href="mailto:r-guseynov@gmail.com"></a><?= $email ?>
+		</dd>
+	</dl>
+	<dl>
+		<dt>Город</dt>
+		<dd><?= $city ?></dd>
+	</dl>
+	<dl>
+		<dt>О себе</dt>
+		<dd><?= $about ?></dd>
+	</dl>	
 </body>
 </html>
